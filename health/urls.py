9c11fabf56ec9f27a3344857ccf5f8e977aecdf9
Anand911 <djakashannad@gmail.com>
2022-06-03 09:30:22 +0530

    added resources and chat-bot fixes

--- health/urls.py
+++ health/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('chat-bot', views.chat_bot, name='chat_bot'),
     path('predict', views.predict, name='predict'),
+    path('home',views.home,name='home')
     #path('save-symp',update_symptoms,name='symptoms-update'),
 ]
